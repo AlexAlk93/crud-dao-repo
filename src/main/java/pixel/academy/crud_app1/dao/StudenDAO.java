a2593bb2250f994f56715ff55567525ab773b6e8
AlexAlk93 <alex.panioglo@gmail.com>
2025-09-23 17:34:39 +0300

    Add class StudentDAOImplimment

---
+++ src/main/java/pixel/academy/crud_app1/dao/StudenDAO.java
@@ -0,0 +1,8 @@
+package pixel.academy.crud_app1.dao;
+
+import pixel.academy.crud_app1.entity.Student;
+
+public interface StudenDAO {
+    void save(Student theStudent);
+}
+
---
+++ src/main/java/pixel/academy/crud_app1/dao/StudentDAOImplimentation.java
@@ -0,0 +1,26 @@
+package pixel.academy.crud_app1.dao;
+
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import pixel.academy.crud_app1.entity.Student;
+
+@Repository
+public class StudentDAOImplimentation implements StudenDAO {
+
+    // Camp pentru EntityManager (va fi utilizat pentru interactiunea cu baza de date)
+    private EntityManager entityManager;
+
+    //Injectarea EntityManager prin constructor
+    @Autowired
+    public StudentDAOImplimentation(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+    //Implimentarea metodei save pentru salvarea unui obiect Student in baza de date
+    @Override
+    @Transactional
+    public void save(Student theStudent) {
+
+    }
+}
